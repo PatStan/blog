935b637236b47be3dc3d4dc1a3864d87a6f12b56
Patrick Stanton <patrickstanton50@gmail.com>
2022-02-09 16:09:35 +0000

    initial html&css

---
+++ resources/views/post.blade.php
@@ -0,0 +1,20 @@
+<!doctype html>
+
+<title>My Blog</title>
+<link rel="stylesheet" href="app.css">
+
+<body>
+
+<article>
+    <h1><a href="/post">First Post</a></h1>
+    <p>
+        shut it hahahahaha literally shut up asiogjfsdkfhgkdjsfgkjrdsgkjdfsbgjkdsfhgkdfjhgkfdjxhgkjdfngjdfbngjkdfxgjdfbnxkjhdfxngk fdbjgkbdfskjghdsfkjbgjksfdhbghjkdfsbgjkdfbgjk
+    </p>
+</article>
+
+<a href ="/"> Go Back</a>
+
+</body>
+
+
+
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,23 @@
+<!doctype html>
+
+<title>My Blog</title>
+<link rel="stylesheet" href="app.css">
+
+<body>
+<h1> buh log</h1>
+
+<article>
+    <h1><a href="/post">First Post</a></h1>
+    <p>
+        shut it hahahahaha literally shut up asiogjfsdkfhgkdjsfgkjrdsgkjdfsbgjkdsfhgkdfjhgkfdjxhgkjdfngjdfbngjkdfxgjdfbnxkjhdfxngk fdbjgkbdfskjghdsfkjbgjksfdhbghjkdfsbgjkdfbgjk
+    </p>
+</article>
+
+<article>
+    <h1><a href="/post">Second Post</a></h1>
+    <p>
+        shut it hahahahaha literally shut up asiogjfsdkfhgkdjsfgkjrdsgkjdfsbgjkdsfhgkdfjhgkfdjxhgkjdfngjdfbngjkdfxgjdfbnxkjhdfxngk fdbjgkbdfskjghdsfkjbgjksfdhbghjkdfsbgjkdfbgjk
+    </p>
+</article>
+
+</body>
