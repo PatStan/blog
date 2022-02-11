881ba614d7165d33ee24a9a647da9c523edfe68c
Patrick Stanton <patrickstanton50@gmail.com>
2022-02-11 14:11:31 +0000

    removed deprecated HTML posts and moved to eloquent

--- routes/web.php
+++ routes/web.php
@@ -26,12 +26,12 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('posts/{post}', function($slug){
+Route::get('posts/{post}', function($id){
 
     //Find a post by its slug and pass it to a view called "post"
 
     return view('post', [
-        'post' => Post::findOrFail($slug)
+        'post' => Post::findOrFail($id)
     ]);
 
 });
