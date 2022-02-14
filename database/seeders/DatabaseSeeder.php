438bae0ad33e008f7cee9c0bc13c4897d96aad8e
Patrick Stanton <patrickstanton50@gmail.com>
2022-02-14 16:54:15 +0000

    post + category factories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,44 +17,49 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        User::truncate();
-        Post::truncate();
-        Category::truncate();
 
-         $user = User::factory()->create();
-
-         $personal = Category::create([
-             'name' => 'Personal',
-             'slug' => 'personal'
-         ]);
-
-         $work = Category::create([
-             'name' => 'Work',
-             'slug' => 'work'
-         ]);
-
-         $hobby = Category::create([
-             'name' => 'Hobby',
-             'slug' => 'hobby'
-         ]);
-
-         Post::create([
-             'user_id' => $user->id,
-             'category_id' => $personal->id,
-             'title' => 'Personal Post',
-             'slug' => 'personal-post',
-             'excerpt' => 'beep boop personal',
-             'body' => 'shut it shut the heck up will you just keep it down i have had enough'
-         ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $work->id,
-            'title' => 'Work Post',
-            'slug' => 'work-post',
-            'excerpt' => 'werk',
-            'body' => 'shut it shut the hecgfsgsfgsdgk up will you just keep it down i have had enough'
+        $user = User::factory()->create([
+            'name' => 'Patrick Stanton'
         ]);
+
+        Post::factory(5)->create([
+            'user_id' => $user->id
+        ]);
+
+//         $user = User::factory()->create();
+//
+//         $personal = Category::create([
+//             'name' => 'Personal',
+//             'slug' => 'personal'
+//         ]);
+//
+//         $work = Category::create([
+//             'name' => 'Work',
+//             'slug' => 'work'
+//         ]);
+//
+//         $hobby = Category::create([
+//             'name' => 'Hobby',
+//             'slug' => 'hobby'
+//         ]);
+//
+//         Post::create([
+//             'user_id' => $user->id,
+//             'category_id' => $personal->id,
+//             'title' => 'Personal Post',
+//             'slug' => 'personal-post',
+//             'excerpt' => 'beep boop personal',
+//             'body' => 'shut it shut the heck up will you just keep it down i have had enough'
+//         ]);
+//
+//        Post::create([
+//            'user_id' => $user->id,
+//            'category_id' => $work->id,
+//            'title' => 'Work Post',
+//            'slug' => 'work-post',
+//            'excerpt' => 'werk',
+//            'body' => 'shut it shut the hecgfsgsfgsdgk up will you just keep it down i have had enough'
+//        ]);
 
     }
 }
