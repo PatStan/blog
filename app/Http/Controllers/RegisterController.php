17ebf8e212560111fcce0c5470f225588824dae6
Patrick Stanton <patrickstanton50@gmail.com>
2022-02-22 14:44:10 +0000

    user registration, no password hashing

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class RegisterController extends Controller
@@ -14,11 +15,16 @@ class RegisterController extends Controller
     public function store()
     {
         //create and store user
-        request()->validate([
-            'name' => ['required', 'max:255'],
-            'username' => ['required', 'max:255', 'min:3'],
+
+        $attributes = request()->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'username' => ['required', 'string', 'max:255', 'min:3'],
             'email' => ['required', 'max:255', 'email'],
             'password' => ['required', 'max:255', 'min:6']
         ]);
+
+        User::create($attributes);
+
+        return redirect('/');
     }
 }
