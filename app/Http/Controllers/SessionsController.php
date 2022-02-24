66398b39040cd2be3c45381c18b1e55621203a4a
Patrick Stanton <patrickstanton50@gmail.com>
2022-02-24 13:17:28 +0000

    login + page

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -3,9 +3,33 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 
 class SessionsController extends Controller
 {
+    public function create()
+    {
+        return view('sessions.create');
+    }
+
+    public function store()
+    {
+        $credentials = request()->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        if (auth()->attempt($credentials))
+        {
+            session()->regenerate();
+            return redirect('/')->with('success', 'Welcome Back :)');
+        }
+
+        throw ValidationException::withMessages([
+            'email' => 'Your credentials could not be verified. Idiot!'
+        ]);
+    }
+
     public function destroy()
     {
         auth()->logout();
