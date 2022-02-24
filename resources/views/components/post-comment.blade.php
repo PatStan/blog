472887828c4d9e03f82bf4589302ed4f30ca86b2
Patrick Stanton <patrickstanton50@gmail.com>
2022-02-24 15:16:51 +0000

    post comment markup

---
+++ resources/views/components/post-comment.blade.php
@@ -0,0 +1,19 @@
+<div>
+    <article class="flex bg-gray-100 border border-gray-200 p-6 rounded-xl space-x-4">
+        <div class="flex-shrink-0">
+            <img src="https://i.pravatar.cc/100" alt="" width="60" height="60" class="rounded-xl">
+        </div>
+
+        <div>
+            <header class="mb-3">
+                <h3 class="font-bold">John Doe</h3>
+
+                <p class="text-xs">
+                    <time>Posted 8 Months Ago</time>
+                </p>
+
+            </header>
+            <p>this is filler make this dynamic! hello my name is john doe the stupid idiot hahahahahahaha</p>
+        </div>
+    </article>
+</div>
