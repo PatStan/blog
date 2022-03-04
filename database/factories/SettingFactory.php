fad96904705c6c95634e7ef8f6e51f4259032a9e
Patrick Stanton <patrickstanton50@gmail.com>
2022-03-04 12:04:20 +0000

    factory and seeder

---
+++ database/factories/SettingFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Setting>
+ */
+class SettingFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'user_id' => User::pluck('id')->random(),
+            'key' => $this->faker->unique()->word(),
+            'value' => $this->faker->word()
+        ];
+    }
+}
