a60fd9ed65d6f2834d6e0bfe81920d14353e4783
Patrick Stanton <patrickstanton50@gmail.com>
2022-03-08 17:36:01 +0000

    authorisation

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,11 @@
 
 namespace App\Providers;
 
+use App\Models\User;
 use App\Services\MailchimpNewsletter;
 use App\Services\Newsletter;
+use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 use MailchimpMarketing\ApiClient;
 use Spatie\FlareClient\Api;
@@ -34,6 +37,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Gate::define('admin', function(User $user){
+            return $user->username === 'test';
+        });
+
+        Blade::if('admin', function() {
+            return request()->user()?->can('admin');
+        });
     }
 }
